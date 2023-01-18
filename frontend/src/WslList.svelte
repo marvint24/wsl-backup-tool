0ca6f18c71fd5c010b5ac1955d6935dcccdfff37
marvint24 <marvin.terme@gmail.com>
2023-01-18 19:31:14 +0100

    scroll for WslList

--- frontend/src/WslList.svelte
+++ frontend/src/WslList.svelte
@@ -5,56 +5,56 @@
 
 </script>
 
-<section>
-  <table>
-    <thead>
-      <tr>
-        <td>Default</td>
-        <td>Name</td>
-        <td>Status</td>
-        <td>WSL-Version</td>
-        <td></td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Default</td>
-        <td>Name</td>
-        <td>Status</td>
-        <td>WSL-Version</td>
-        <td></td>
-      </tr>
-      {#each $distros as distroRow (distroRow.uuid)}
-        <DistroRow distroRow={distroRow}/>
-      {/each}
-    </tbody>
-  </table>
-</section>
+<div>
+  <section>
+    <table>
+        <tr>
+            <td>Default</td>
+            <td>Name</td>
+            <td>Status</td>
+            <td>WSL-Version</td>
+            <td></td>
+        </tr>
+        {#each $distros as distroRow (distroRow.uuid)}
+            <DistroRow distroRow={distroRow}/>
+        {/each}
+    </table>
+  </section>
+</div>
 
 <style>
-tbody{
-  max-height: 400px;
+section {
   overflow: auto;
-}
-thead, tbody { 
-  display: block;
+  max-height: calc(100vh - 97px);
 }
 table{
-    width: 95%;
-    margin-left: auto;
-    margin-right: auto;
-    border-spacing: 0 10px; 
-}
-thead>tr>td{
-  width: 20%;
+  width: 97%;
+  margin-left: auto;
+  margin-right: auto;
+  border-spacing: 0 10px;
 }
 td{
-    text-align: left;
-    color: var(--white);
-    font-size: 20px;
-    width: 20%
+  text-align: left;
+  color: var(--white);
+  font-size: 20px;
 }
 tr td:first-child , tr td:nth-child(4){
-    text-align: center;
+  text-align: center;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: hwb(0 0% 100% / 0);
+}
+
+::-webkit-scrollbar-thumb {
+  background: var(--white);
+  border-radius: 5px;
+}
+div{
+  padding: 0 8px 0 8px;
 }
 </style>
