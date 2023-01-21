881bd8243b252e6aab4a369a277dda681347eac6
marvint24 <marvin.terme@gmail.com>
2023-01-21 16:02:30 +0100

    added pics

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:     "wsl-backup-tool",
+		Title:     "WSL Backup Tool",
 		Width:     1024,
 		Height:    768,
 		MinWidth:  768,
